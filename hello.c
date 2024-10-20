3f90d82cfe62d1f220a1f3366172c63f801ceefc
mazdiaz <46370875+mazdiaz@users.noreply.github.com>
2024-10-20 14:20:39 +0700

    new printArr function

--- hello.c
+++ hello.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void printArr(int *arr, int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
 int main() {
 
-    printf("Hello Wolrd!\n");
+    int arr[] = {1, 3, 2, 5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printArr(arr, n);
 
     return 0;
 }
